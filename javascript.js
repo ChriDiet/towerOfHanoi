76fb68dacbf72f0a274f33a15dedfe7bcc6d03bc
Ticster <empynia@gmail.com>
2025-02-28 08:26:57 +0100

    about finished

--- javascript.js
+++ javascript.js
@@ -7,42 +7,73 @@ let largeDiskTowerIndex = 0;
 
 let disableButtons = [true, false, false, true, true, true, true, true, true];
 
+const mediumButtonsRangeOffset = 3;
+const largeButtonsRangeOffset = 6;
+
+let solved = false;
+
 // view 
 updateView();
 
 function updateView() {
-   document.getElementById('app').innerHTML = /*HTML*/ `
-         <div class="game-container">
-            ${createTowerHtml(0)}
-            ${createTowerHtml(1)}
-            ${createTowerHtml(2)}
-         </div>
-            Flytt liten disk til 
-             <button onclick="moveSmallDisk(0)" ${disableButtons[0] ? 'disabled' : ''}>venstre tårn</button>
-             
-            <button onclick="moveSmallDisk(1)" ${disableButtons[1] ? 'disabled' : ''}>midtre tårn</button>
-            <button onclick="moveSmallDisk(2)" ${disableButtons[2] ? 'disabled' : ''}>høyre tårn</button>
-            <br/>
-            Flytt medium disk
-            <button onclick="moveMediumDisk(0)" ${disableButtons[3] ? 'disabled' : ''}>venstre tårn</button>
-            <button onclick="moveMediumDisk(1)" ${disableButtons[4] ? 'disabled' : ''}>midtre tårn</button>
-            <button onclick="moveMediumDisk(2)" ${disableButtons[5] ? 'disabled' : ''}>høyre tårn</button>
-            <br/>
-            Flytt stor disk
-            <button onclick="moveLargeDisk(0)" ${disableButtons[6] ? 'disabled' : ''}>venstre tårn</button>
-            <button onclick="moveLargeDisk(1)" ${disableButtons[7] ? 'disabled' : ''}>midtre tårn</button>
-            <button onclick="moveLargeDisk(2)" ${disableButtons[8] ? 'disabled' : ''}>høyre tårn</button>
-            <br/>
-      `;
-}
+   !solved
+      ? document.getElementById('app').innerHTML = /*HTML*/ `
+      <div class="game-container">
+         ${createTowerHtml(0)}
+         ${createTowerHtml(1)}
+         ${createTowerHtml(2)}
+      </div>
 
+   Flytt liten disk til 
+   ${createButtonsHtml('moveSmallDisk')}
 
-function createBtnHtml(moveFunc, index) {
-   return /*html*/ `
-      <button onclick="${moveFunc(index)}" ${smallDisable[index] ? 'disabled' : ''}>venstre tårn</button>
+   
+   <br/>
+   Flytt medium disk
+   ${createButtonsHtml('moveMediumDisk')}
+   <br/>
+   Flytt stor disk
+   ${createButtonsHtml('moveLargeDisk')}
+   <br/>
+      `
+      : document.getElementById('app').innerHTML = /*html*/ `
+      <div>Congratulations</div>
+      ${createButtonHtml('restart', '', 'Start nytt spill')}
    `;
 }
 
+// set in controller or in view?
+function restart() {
+   smallDiskTowerIndex = 0;
+   mediumDiskTowerIndex = 0;
+   largeDiskTowerIndex = 0;
+   solved = false;
+   updateView();
+}
+
+function createButtonsHtml(diskType) {
+   const labels = ['venstre tårn', 'midtre tårn', 'høyre tårn'];
+   let buttonHtml = ``;
+
+   labels.forEach((label, index) => {
+      buttonHtml += createButtonHtml(diskType, index, label)
+   })
+
+   return buttonHtml;
+}
+function createButtonHtml(onclick, index, label) {
+   return /*html*/ `
+      <button onclick="${onclick}(${index})" ${isDisabled(onclick, index)}>
+         ${label}
+      </button>
+   `;
+}
+function isDisabled(type, index) {
+   if (type === 'moveSmallDisk') return disableButtons[index] ? 'disabled' : '';
+   if (type === 'moveMediumDisk') return disableButtons[index + mediumButtonsRangeOffset] ? 'disabled' : '';
+   if (type === 'moveLargeDisk') return disableButtons[index + largeButtonsRangeOffset] ? 'disabled' : '';
+
+}
 
 function drawDisks(pos) {
    return /*html*/ `
@@ -82,19 +113,19 @@ function createTowerHtml(towerIndex) {
 // controller
 function moveSmallDisk(toTowerIndex) {
    smallDiskTowerIndex = checkDiskIndex(toTowerIndex);
-   canMove();
+   canMoveDisk();
    updateView();
 }
 
 function moveMediumDisk(toTowerIndex) {
    mediumDiskTowerIndex = checkDiskIndex(toTowerIndex);
-   canMove();
+   canMoveDisk();
    updateView();
 }
 
 function moveLargeDisk(toTowerIndex) {
    largeDiskTowerIndex = checkDiskIndex(toTowerIndex);
-   canMove();
+   canMoveDisk();
    updateView();
 }
 
@@ -120,33 +151,32 @@ function disableBtn(btnType, index, value) {
    btnType[index] = value;
 }
 
-function canMove() {
+function canMoveDisk() {
    const pos = [0, 1, 2];
-   const mediumButton = 3;
-   const largeButton = 6;
-
    let state = getState();
 
 
    for (let i = 0; i < state.length; i++) {
-
-
       const isSmallDisabled = state[0] === pos[i];
       disableBtn(disableButtons, pos[i], isSmallDisabled);
 
       const isMediumDisabled = state.slice(0, 2).includes(pos[i]);
-      disableBtn(disableButtons, pos[i] + mediumButton, isMediumDisabled);
+      disableBtn(disableButtons, pos[i] + mediumButtonsRangeOffset, isMediumDisabled);
 
       const isLargeDisabled = state.slice(0, 3).includes(pos[i]);
-      disableBtn(disableButtons, pos[i] + largeButton, isLargeDisabled);
+      disableBtn(disableButtons, pos[i] + largeButtonsRangeOffset, isLargeDisabled);
 
-      if (state[0] === state[1]) {
-         disableBtn(disableButtons, pos[i] + mediumButton, true);
-      }
+      if (state[0] === state[1]) disableBtn(disableButtons, pos[i] + mediumButtonsRangeOffset, true);
 
-      if (state[0] === state[2] || state[1] === state[2]) {
-         disableBtn(disableButtons, pos[i] + largeButton, true);
-      }
-
+      if (state[0] === state[2] || state[1] === state[2]) disableBtn(disableButtons, pos[i] + largeButtonsRangeOffset, true);
    }
+   solvedHanoiGame();
+}
+
+
+function solvedHanoiGame() {
+   const state = getState();
+
+   const isSolved = (diskPositions) => diskPositions === 2;
+   state.every(isSolved) ? solved = true : false;
 }
